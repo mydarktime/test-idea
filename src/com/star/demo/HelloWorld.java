43df0b3a40d51de0cdfd012d57c536fa0ff40bf7
tom_glb <goodMorning_glb@star.com>
2021-05-18 08:31:04 +0800

    提交修改过的helloworld

--- src/com/star/demo/HelloWorld.java
+++ src/com/star/demo/HelloWorld.java
@@ -3,5 +3,6 @@ package com.star.demo;
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("hello world");
+        System.out.println("新增一行");
     }
 }
