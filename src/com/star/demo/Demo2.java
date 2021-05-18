6fae319bca6df8a5a3ac94cc5ef7a4b644068b97
tom_glb <goodMorning_glb@star.com>
2021-05-18 09:33:56 +0800

    将Demo2提交到本地库

---
+++ src/com/star/demo/Demo2.java
@@ -0,0 +1,13 @@
+package com.star.demo;
+
+/**
+ * @Auther: zhangskystar
+ * @Date: 2021-05-18-05-18-9:32
+ * @Description: com.star.demo
+ * @Version: 1.0
+ */
+public class Demo2 {
+    public static void main(String[] args) {
+        System.out.println("这是测试在idea上执行push");
+    }
+}
