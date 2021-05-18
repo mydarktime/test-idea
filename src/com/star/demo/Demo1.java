a3b5a48a79a696fb219f92a27f3c18144822e6b3
tom_glb <goodMorning_glb@star.com>
2021-05-18 08:33:54 +0800

    提交暂存区中的Demo1

---
+++ src/com/star/demo/Demo1.java
@@ -0,0 +1,17 @@
+package com.star.demo;
+
+public class Demo1 {
+    private String name;
+    private int age;
+
+    public Demo1() {
+    }
+
+    public Demo1(String name) {
+        this.name = name;
+    }
+
+    public Demo1(int age) {
+        this.age = age;
+    }
+}
